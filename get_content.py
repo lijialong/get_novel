7abbf10faa779fd8b75bbb85d3b5f4a0e8bb46f2
Leif <919820268@qq.com>
2019-01-26 16:50:55 +0800

    第一次修改

--- get_content.py
+++ get_content.py
@@ -1,27 +1,38 @@
 # 用于获取小说内容
 from requests_html import HTMLSession
 import re
-import asyncio
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+# import time
 
 def get_content(url):
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    doCode ='''
-        (function(){
-            var bp = document.createElement('script');
-            bp.src = '//push.zhanzhang.baidu.com/push.js';
-            var s = document.getElementsByTagName("script")[0];
-            s.parentNode.insertBefore(bp, s);
-        })();
-    '''
-    session = HTMLSession()
-    rep = session.get(url=url)
-    temp = rep.html
-    temp.render(script=doCode)
-    temp.encoding = 'utf-8'
+    # doCode ='''
+    #     (function(){
+    #         var bp = document.createElement('script');
+    #         bp.src = '//push.zhanzhang.baidu.com/push.js';
+    #         var s = document.getElementsByTagName("script")[0];
+    #         s.parentNode.insertBefore(bp, s);
+    #     })();
+    # '''
+    # session = HTMLSession()
+    # rep = session.get(url=url)
+    # temp = rep.html
+    # temp.render(script=doCode)
+    # temp.encoding = 'utf-8'
+
+    driver = webdriver.PhantomJS(executable_path="./phantomjs.exe",service_args=['--ignore-ssl-errors=true', '--ssl-protocol=TLSv1'])
+    driver.get(url)
+    wait = WebDriverWait(driver, 10)
+    wait.until(
+        EC.presence_of_element_located(
+            (By.XPATH, '//div[@id="content"]')))
+    html = driver.page_source
+
     re_str = r"<div id=\"content\">(.*?)</div>"
     re_class = re.compile(re_str)
-    replay = re.findall(re_class,str(temp.html).replace("\n",""))
-    # print(temp.html)
+    replay = re.findall(re_class,str(html).replace("\n",""))
     return replay[0].replace("&nbsp;&nbsp;&nbsp;&nbsp;","   ").replace("<br><br>","\n")
 
 # print(get_content("http://www.biquyun.com/19_19420/9010087.html"))
--- get_main.py
+++ get_main.py
@@ -2,39 +2,43 @@ import get_content
 import get_url
 import threading
 import re
-import random
+# import random
 
 url = "http://www.biquyun.com/19_19420/"    #指定小说在笔趣阁中的url
 path = "D:\\资料\\scrpylearn\\爬取小说\\"   #指定小说的存放路径
 
 title = get_url.get_url(url)
-
-def save_novel(urlpath):
-    for up in urlpath:
-        re_str = r"(.*?)\""
-        re_class = re.compile(re_str)
-        replay = re.findall(re_class,up)
-        novel_title = up.replace(replay[0],"").replace("\">","")
-        novel_url = url + replay[0]
-        with open(path+novel_title+".txt","w") as f:
-            f.write(get_content.get_content(novel_url))
-            f.close
+n = 0
+# chinese_word = {'一':'1','二':'2','三':'3','四':'4','五':'5','六':'6','七':'7','八':'8','九':'9'}
+for up in title:
+    re_str = r"(.*?)\""
+    re_class = re.compile(re_str)
+    replay = re.findall(re_class,up)
+    novel_title = up.replace(replay[0],"").replace("\">","").replace(" ","")
+    # for n in chinese_word:
+    #     novel_title = novel_title.replace(n,chinese_word[n])
+    novel_url = url + replay[0]
+    with open(path+str(n)+novel_title+".txt","w") as f:
+        f.write(get_content.get_content(novel_url))
+        f.close
+    n+=1
+    print('>>>>>>已爬取：%s<<<<<<'%(novel_title))
 
 # 多线程爬取
-threads=[]
+# threads=[]
 
-for i in range(0,10):
-    exec("temp%s = []"%(str(i)))
+# for i in range(0,10):
+#     exec("temp%s = []"%(str(i)))
 
-for ur in title:
-    exec("temp%s.append('%s')"%(str(random.randint(0,9)),ur))
+# for ur in title:
+#     exec("temp%s.append('%s')"%(str(random.randint(0,9)),ur))
 
-for i in range(0,10):
-    exec("t%s = threading.Thread(target=save_novel,args=(temp%s,))"%(str(i),str(i)))
-    exec("threads.append(t%s)"%(str(i)))
+# for i in range(0,10):
+#     exec("t%s = threading.Thread(target=save_novel,args=(temp%s,))"%(str(i),str(i)))
+#     exec("threads.append(t%s)"%(str(i)))
 
-for thraed in threads:
-    thraed.start()
+# for thraed in threads:
+#     thraed.start()
 
-for thraed in threads:
-    thraed.join()
+# for thraed in threads:
+#     thraed.join()
